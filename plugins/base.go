e0545def056793aed7c7526e47c243d88521521f
Mino <mino@minomino.org>
2016-12-13 05:39:17 +0100

    base: Added Chrome and Safari user agents.

--- plugins/base.go
+++ plugins/base.go
@@ -39,6 +39,8 @@ import (
 const (
 	FirefoxUserAgent = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)"
 	IE11UserAgent    = "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko"
+	ChromeUserAgent  = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"
+	SafariUserAgent  = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A"
 )
 
 // Implements the error interface.
