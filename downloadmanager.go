f9003f5fe459d31bab65f57f00523b4abeaca394
Mino <mino@minomino.org>
2016-12-21 04:56:56 +0100

    Made "Disable" special option get parsed before all other special options.

--- downloadmanager.go
+++ downloadmanager.go
@@ -305,22 +305,6 @@ func (dm *DownloadManager) Download(url string, maxWorkers int, zipit, override 
 
 	if !override {
 		special := GetSpecialOptions(dm.plugin)
-		if z, ok := special["Zip"]; ok {
-			if zipit, ok = z.Value().(bool); !ok {
-				log.Error("Special option 'Zip' was not a bool.")
-				panic(ErrInvaidSpecialOptionType)
-			}
-
-			log.Warnf("This plugin forces the --zip flag to %v.", zipit)
-		}
-		if w, ok := special["Workers"]; ok {
-			if maxWorkers, ok = w.Value().(int); !ok {
-				log.Error("Special option 'Workers' was not an int.")
-				panic(ErrInvaidSpecialOptionType)
-			}
-
-			log.Warnf("This plugin forces the --workers flag to %d.", maxWorkers)
-		}
 		if disable, ok := special["Disable"]; ok {
 			if _, ok = disable.Value().(bool); !ok {
 				log.Error("Special option 'Disable' was not an bool.")
@@ -331,6 +315,24 @@ func (dm *DownloadManager) Download(url string, maxWorkers int, zipit, override 
 				log.Warn("Plugin disabled: " + comment)
 			}
 			return nil, ErrDisabled
+		}
+
+		if z, ok := special["Zip"]; ok {
+			if zipit, ok = z.Value().(bool); !ok {
+				log.Error("Special option 'Zip' was not a bool.")
+				panic(ErrInvaidSpecialOptionType)
+			}
+
+			log.Warnf("This plugin forces the --zip flag to %v.", zipit)
+		}
+
+		if w, ok := special["Workers"]; ok {
+			if maxWorkers, ok = w.Value().(int); !ok {
+				log.Error("Special option 'Workers' was not an int.")
+				panic(ErrInvaidSpecialOptionType)
+			}
+
+			log.Warnf("This plugin forces the --workers flag to %d.", maxWorkers)
 		}
 	}
 
